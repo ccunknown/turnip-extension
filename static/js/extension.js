8f1e23b4f2ea608882b27212f1463ebef25a8661
ccunknown <pakawat_tinsirisuk@hotmail.com>
2020-03-06 18:44:44 +0700

    first commit

--- static/js/extension.js
+++ static/js/extension.js
@@ -220,6 +220,30 @@
           }
         }
       }
+
+      this.renderContentWebhook();
+    }
+
+    renderContentWebhook() {
+      console.log(`renderContentWebhook() >> `);
+      // create the editor
+      const container = document.getElementById("jsoneditor");
+      const options = {};
+      const editor = new JSONEditor(container, options);
+
+      // set json
+      const initialJson = {
+        "Array": [1, 2, 3],
+        "Boolean": true,
+        "Null": null,
+        "Number": 123,
+        "Object": {"a": "b", "c": "d"},
+        "String": "Hello World"
+      };
+      editor.set(initialJson);
+
+      // get json
+      const updatedJson = editor.get();
     }
 
     generateToken(token) {
